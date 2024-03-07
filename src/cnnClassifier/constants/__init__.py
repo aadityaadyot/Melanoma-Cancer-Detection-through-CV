beddf20a7db8b5e4521c3923cd000e75597dc8ed
aadityaadyot <aadityaadyot@gmail.com>
2024-03-08 02:31:42 +0530

    callbacks defined

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
 CONFIG_FILE_PATH = Path("/config/workspace/config/config.yaml")
-PARAMS_FILE_PATH = Path("params.yaml")
+PARAMS_FILE_PATH = Path("/config/workspace/params.yaml")
